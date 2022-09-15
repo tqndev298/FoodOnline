757f394012f84fc3fbf80d12c0b9e96bc32e5277
I am TQN <uznaruto298@gmail.com>
2022-09-15 22:09:50 +0700

    customer and vendor registration

--- accounts/models.py
+++ accounts/models.py
@@ -79,6 +79,13 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
 
+    def get_role(self):
+        if self.role == 1:
+            user_role = 'Vendor'
+        elif self.role == 2:
+            user_role = 'Customer'
+        return user_role
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(
@@ -100,6 +107,3 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.email
-
-
-
