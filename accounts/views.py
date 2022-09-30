b4ef9935f621bf1553dab853f330ba3ec70f1bad
I am TQN <uznaruto298@gmail.com>
2022-09-30 22:35:09 +0700

    marketplace show simple

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from accounts.utils import detectUser, send_verification_email
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
+from django.template.defaultfilters import slugify
 
 
 from vendor.forms import VendorForm
@@ -101,6 +102,8 @@ def registerVendor(request):
             user.save()
             vendor = v_form.save(commit=False)
             vendor.user = user
+            vendor_name = v_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
@@ -186,7 +189,7 @@ def custDashboard(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
-    
+
     return render(request, 'accounts/vendorDashboard.html')
 
 
--- menu/models.py
+++ menu/models.py
@@ -12,7 +12,6 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
@@ -26,7 +25,8 @@ class Category(models.Model):
 
 class FoodItem(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='fooditems')
     food_title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=250, blank=True)
     description = models.TextField(max_length=250, blank=True)
